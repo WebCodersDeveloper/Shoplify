31b588efa41efe0255fb73484310e0ba7ae049f9
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-11-09 17:02:13 +0500

    s

--- src/components/Add/AddProduct.jsx
+++ src/components/Add/AddProduct.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React from "react";
+import "../Add/addproduct.scss";
 
-export default function AddProduct() {
+export default function AddProduct({
+  name,
+  setName,
+  price,
+  setPrice,
+  category, 
+  setCategory, 
+  creatProduct,
+  product,
+  removeItem,
+}) {
   return (
-    <div>AddProduct</div>
-  )
+    <>
+      <div className="container">
+        <div className="newItem">
+          <form action="" onSubmit={creatProduct}>
+            <input
+              type="text"
+              placeholder="Название продукта"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Цена"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+
+            <label class="select">
+              <select
+                value={category} 
+                onChange={(e) => setCategory(e.target.value)} 
+              >
+                <option value="Категория" selected disabled>
+                  Категория
+                </option>
+                <option value="Молочные продукты">Молочные продукты</option>
+                <option value="napitki">Напитки</option>
+                <option value="xleb">Хлеб</option>
+                <option value="frukti">Фрукты</option>
+                <option value="ovoshi">Овощи</option>
+                <option value="zamorojennie">Замороженные</option>
+                <option value="konditerskie izdeliya">
+                  Кондитерские изделия
+                </option>
+                <option value="chay,kofe">Чай, Кофе</option>
+                <option value="bakaleya">Бакалея</option>
+                <option value="zdorovoe pitanie">Здоровое питание</option>
+                <option value="zootovari">Зоотовары</option>
+                <option value="detskoe pitanie">Детское питание</option>
+                <option value="myaso, kolbasa">Мясо, Колбаса</option>
+                <option value="neprodovolstvennie tovari">
+                  Непродовольственные товары
+                </option>
+              </select>
+            </label>
+
+            <button disabled={!name || !price} onClick={creatProduct}>
+              Добавить
+            </button>
+          </form>
+        </div>
+        <div className="itemList">
+          {product.map((item) => {
+            return (
+              <div className="item" key={item.id}>
+                <img src={item.image} alt="image" />
+                <div className="txt">
+                  <h3>{item.name}</h3>
+                  <p>{item.narx} Сум</p>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Aliquam, excepturi nesciunt atque iure corrupti eligendi!
+                  </p>
+                  <div className="btn_cont">
+                    <button
+                      onClick={() => removeItem(item.id)}
+                      className="trash_btn"
+                    >
+                      <i class="fa-sharp fa-regular fa-trash-can fa-beat-fade"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
